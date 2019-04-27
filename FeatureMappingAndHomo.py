8216998ea96053b350ea28afca5e6f7699f343b5
Benedictionary <summitless@gmail.com>
2019-04-27 13:17:18 -0700

    Still Testing Code

--- FeatureMappingAndHomo.py
+++ FeatureMappingAndHomo.py
@@ -1,40 +1,43 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 27 10:58:02 2019
-
-@author: eagle
-"""
+#ECE 5470 - Final Project - Gordon Keil & Ben CHeng
 
 import numpy as np
 import cv2
+
 imgname = "SingleMcDonaldsFries.jpg"          # query image (small object)
 imgname2 = "MultipleMcDonaldsFries.jpg" # train image (large scene)
 
-MIN_MATCH_COUNT = 4
-
-## Create ORB object and BF object(using HAMMING)
+## Create ORB object
 orb = cv2.ORB_create()
 img1 = cv2.imread(imgname)
 img2 = cv2.imread(imgname2)
 
-gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+#Change Colorspace from BGR to Grayscale
 gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
 ## Find the keypoints and descriptors with ORB
 kpts1, descs1 = orb.detectAndCompute(gray1,None)
 kpts2, descs2 = orb.detectAndCompute(gray2,None)
 
 ## match descriptors and sort them in the order of their distance
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-matches = bf.match(descs1, descs2)
-dmatches = sorted(matches, key = lambda x:x.distance)
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True) #bf = created Brute-Force object. BFMatcher = Brute-Force Matcher
+matches = bf.match(descs1, descs2) #finds matches in the descrripters for set [arg1] and set [arg2]
+dmatches = sorted(matches, key = lambda x:x.distance) #sorts the matches based on their distances
 
+# Lets display the first 15 "best" matches
+imgTestMatches = cv2.drawMatches(img1, kpts1, img2, kpts2, dmatches[:20], None, flags=2)
+imgTestH, imgTestW = imgTestMatches.shape[:2]
+imgTestMatchesResized = cv2.resize(imgTestMatches, (int(imgTestW / 2.7), int(imgTestH / 2.7))) #outputs the resized image
+cv2.imshow("Top Matches", imgTestMatchesResized)
+
+'''
 ## extract the matched keypoints
 src_pts  = np.float32([kpts1[m.queryIdx].pt for m in dmatches]).reshape(-1,1,2)
 dst_pts  = np.float32([kpts2[m.trainIdx].pt for m in dmatches]).reshape(-1,1,2)
 
 ## find homography matrix and do perspective transform
-M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0) #Note the homography Matrix M needs at least 4 matches
+print("Matrix M", M)
 h,w = img1.shape[:2]
 pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
 dst = cv2.perspectiveTransform(pts,M)
@@ -48,12 +51,12 @@ cv2.imshow("found", img2)
 
 ## draw match lines
 res = cv2.drawMatches(img1, kpts1, img2, kpts2, dmatches[:20],None,flags=2)
-
 resH, resW = res.shape[:2]
 
-resized = cv2.resize(res, (int(resW / 2.7), int(resH / 2.7)))
+#Resize Image for Easy Viewing
+resized = cv2.resize(res, (int(resW / 2.7), int(resH / 2.7))) #outputs the resized image
 
-cv2.imshow("orb_match", resized);
-
+cv2.imshow("ORB Matches", resized)
+'''
 cv2.waitKey()
 cv2.destroyAllWindows()
