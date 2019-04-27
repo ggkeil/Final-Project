d7303240d92a546dec71c07dee0c7a759dbba346
ggkeil <ggkeil@cpp.edu>
2019-04-27 11:48:16 -0700

    Initial commit

---
+++ ORBExample.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 27 10:21:05 2019
+
+@author: eagle
+"""
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('simple.jpg',0)
+
+# Initiate STAR detector
+orb = cv2.ORB_create(10000)
+
+# find the keypoints with ORB
+kp = orb.detect(img, None)
+
+# compute the descriptors with ORB
+kp, des = orb.compute(img, kp)
+
+# draw only keypoints location,not size and orientation
+img2 = cv2.drawKeypoints(img,kp, None, color=(0,255,0), flags=0)
+plt.imshow(img2),plt.show()
