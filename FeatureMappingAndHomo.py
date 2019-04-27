1ca72ab218e2182f87bc32b0d7f69cb6ca9bebb2
ggkeil <ggkeil@cpp.edu>
2019-04-27 14:24:25 -0700

    Update FeatureMappingAndHomo.py

--- FeatureMappingAndHomo.py
+++ FeatureMappingAndHomo.py
@@ -7,12 +7,9 @@ Created on Sat Apr 27 10:58:02 2019
 
 import numpy as np
 import cv2
-<<<<<<< HEAD
 
 imgname = "AmtrakAngled.jpg"          # query image (small object)
 imgname2 = "AmtrakRef.jpg"            # train image (large scene)
-=======
->>>>>>> parent of b463f3d... Added More Comments
 
 MIN_MATCH_COUNT = 4
 
@@ -38,9 +35,10 @@ src_pts  = np.float32([kpts1[m.queryIdx].pt for m in dmatches]).reshape(-1,1,2)
 dst_pts  = np.float32([kpts2[m.trainIdx].pt for m in dmatches]).reshape(-1,1,2)
 
 ## find homography matrix and do perspective transform
-M = cv2.getPerspectiveTransform(src_pts, dst_pts)
-
-dst = cv2.warpPerspective(img1, M, (300,300))
+M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+h,w = img1.shape[:2]
+pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+dst = cv2.perspectiveTransform(pts,M)
 
 h2, w2 = img2.shape[:2]
 
@@ -54,11 +52,8 @@ res = cv2.drawMatches(img1, kpts1, img2, kpts2, dmatches[:20],None,flags=2)
 
 resH, resW = res.shape[:2]
 
-<<<<<<< HEAD
 # resize the resulting image
 resized = cv2.resize(res, (int(resW / 3.3), int(resH / 3.3)))
-=======
->>>>>>> parent of b463f3d... Added More Comments
 
 cv2.imshow("orb_match", resized);
 
