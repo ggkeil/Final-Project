5ac575d15f2c58301a9c6ef038f41fe57cf6997a
ggkeil <ggkeil@cpp.edu>
2019-04-27 17:38:08 -0700

    Day1

--- FeatureMappingAndHomo.py
+++ FeatureMappingAndHomo.py
@@ -8,54 +8,106 @@ Created on Sat Apr 27 10:58:02 2019
 import numpy as np
 import cv2
 
-imgname = "AmtrakRef.jpg"          # query image (small object)
-imgname2 = "AmtrakAngled.jpg"            # train image (large scene)
+imgname = "WhiteBoard1.jpg"             # query image (small object)
+imgname2 = "WhiteBoard2.jpg"            # train image (large scene)
 
-MIN_MATCH_COUNT = 4
-
-## Create ORB object and BF object(using HAMMING)
-orb = cv2.ORB_create()
 img1 = cv2.imread(imgname)
 img2 = cv2.imread(imgname2)
 
-gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+MAX_FEATURES = 500
 
-## Find the keypoints and descriptors with ORB
-kpts1, descs1 = orb.detectAndCompute(gray1,None)
-kpts2, descs2 = orb.detectAndCompute(gray2,None)
+def alignImages(img1, img2, numGoodMatches):
+    ## Create ORB object and BF object(using HAMMING)
+    # BF: Brute Force
+    # Alternative to BF is FLANN
+    orb = cv2.ORB_create(MAX_FEATURES)
+    # Convert images to grayscale
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    
+    ## Find the keypoints and descriptors with ORB
+    kpts1, descs1 = orb.detectAndCompute(gray1,None)
+    kpts2, descs2 = orb.detectAndCompute(gray2,None)
+    
+    ## match descriptors and sort them in the order of their distance
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True) # Brute Force Matching
+    matches = bf.match(descs1, descs2) # brute force match with the two descriptors found earlier
+    dmatches = sorted(matches, key = lambda x:x.distance) # where the sorting happens
+    
+    ## extract the matched keypoints
+    # the matched keypoints are in numpy float32 arrays
+    src_pts  = np.float32([kpts1[m.queryIdx].pt for m in dmatches]).reshape(-1,1,2)
+    dst_pts  = np.float32([kpts2[m.trainIdx].pt for m in dmatches]).reshape(-1,1,2)
+    
+    ## find homography matrix and do perspective transform
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0) # M is the homography matrix
+                                                                   # not doing anything with mask at the moment
+    h,w = img1.shape[:2]
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts, M)
+    
+    # Draw found regions
+    img2 = cv2.polylines(img2, [np.int32(dst)], True, (0,0,255), 1, cv2.LINE_AA)
+    cv2.imshow("found", img2)
+    
+    ## draw match lines
+    res = cv2.drawMatches(img1, kpts1, img2, kpts2, dmatches[:20],None,flags=2)
+    
+    resH, resW = res.shape[:2]
+    
+    # resize the resulting image to fit on laptop screen
+    resized = cv2.resize(res, (int(resW / 3.3), int(resH / 3.3)))
+    
+    # show the resulting image
+    cv2.imshow("orb_match", resized)
+    
+    # Use homography
+    height, width = img2.shape[:2]
+    height = height * 2
+    width = width * 2
+    final = cv2.warpPerspective(img1, M, (width, height))
+    final[0:img2.shape[0], 0:img2.shape[1]] = img2
+    
+    cv2.namedWindow('Final Image', cv2.WINDOW_NORMAL)
+    cv2.imshow("Final Image", final)
+    
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    
+alignImages(img1, img2, 4)
 
-## match descriptors and sort them in the order of their distance
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-matches = bf.match(descs1, descs2)
-dmatches = sorted(matches, key = lambda x:x.distance)
 
-## extract the matched keypoints
-src_pts  = np.float32([kpts1[m.queryIdx].pt for m in dmatches]).reshape(-1,1,2)
-dst_pts  = np.float32([kpts2[m.trainIdx].pt for m in dmatches]).reshape(-1,1,2)
 
+
+
+
+'''
 ## find homography matrix and do perspective transform
-M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-h,w = img1.shape[:2]
+M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0) # M is the homography matrix
+                                                               # not doing anything with mask at the moment
+h,w = img1.shape[:2] # height and width of the query image
 pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
 dst = cv2.perspectiveTransform(pts,M)
 
-h2, w2 = img2.shape[:2]
+h2, w2 = img2.shape[:2] # height and width of the train image
 
 ## draw found regions
-img2 = cv2.polylines(img2, [np.int32(dst)], True, (0,0,255), 1, cv2.LINE_AA)
-resizedimg2 = cv2.resize(img2, (int(w2 / 1.5), int(h2 / 1.5)))
-cv2.imshow("found", img2)
+img2 = cv2.polylines(img2, [np.int32(dst)], True, (0,0,255), 1, cv2.LINE_AA) # draw on the train image
+resizedimg2 = cv2.resize(img2, (int(w2 / 1.5), int(h2 / 1.5))) # resize the found regions to fit the screen
+cv2.imshow("found", img2) # show the found region
 
 ## draw match lines
+# Basically, the similar features are pointed out
 res = cv2.drawMatches(img1, kpts1, img2, kpts2, dmatches[:20],None,flags=2)
 
 resH, resW = res.shape[:2]
 
-# resize the resulting image
+# resize the resulting image to fit on laptop screen
 resized = cv2.resize(res, (int(resW / 3.3), int(resH / 3.3)))
 
-cv2.imshow("orb_match", resized);
+# show the resulting image
+cv2.imshow("orb_match", resized)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
+'''
