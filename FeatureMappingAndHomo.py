d3b6de7d9edcb62eaa6a87e131c94712741c84dc
Benedictionary <summitless@gmail.com>
2019-05-01 16:24:04 -0700

    Removed Unnecessary Stuff from Code

--- FeatureMappingAndHomo.py
+++ FeatureMappingAndHomo.py
@@ -100,8 +100,6 @@ def HSV_Equalize(image):
 img1Loc = r'Trump_Middle.jpg'
 img2Loc = r'Trump_Right.jpg'
 img3Loc = r'Trump_Left.jpg'
-imgOutLoc = 'imgOut1.jpg'
-imgOutLoc2 = 'imgOut2.jpg'
 #img1Loc = r'Zion_Right.jpg'
 #img2Loc = r'Zion_Middle.jpg'
 #img3Loc = r'Zion_Left.jpg'
@@ -138,21 +136,10 @@ def imageStitcher(img1Location, img2Location):
     ## find homography matrix and do perspective transform
     M, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0) #Note the homography Matrix M needs at least 4 matches
     print("Matrix M", M)
-    h,w = img1.shape[:2]
-    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-    #dst = cv2.perspectiveTransform(pts,M)
-    #img2 = cv2.perspectiveTransform(pts, M)
-    #print("Dst: ", dst)
 
     ## #Draw the lines of where Image 1 "should be" on Image 2.
     #img2 = cv2.polylines(img2, [np.int32(dst)], True, (180,105,255), 10, cv2.LINE_AA)
     img2H, img2W = img2.shape[:2]
-
-    '''
-    cv2.namedWindow('found', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('found', int(img2W/8) , int(img2H/8))
-    cv2.imshow("found", img2)
-    '''
 
     ## Create an single image with both Img1 and Img2 side-by-side, with the matches drawn in between them.
     res = cv2.drawMatches(img1, kpts1, img2, kpts2, dmatches[:20],None,flags=2)
@@ -182,13 +169,10 @@ cv2.imwrite(imgOutLoc2, imgOut2)
 # End of stitching part
 imgCrop = cv2.imread(imgOutLoc2)
 
-print("Hello World")
-
 # Show the cropped and sitched image
 cropped = crop(imgOut1)
 cv2.namedWindow('After Cropping', cv2.WINDOW_NORMAL )
 cv2.imshow("After Cropping", cropped)
-
 
 # Should the image be HSV Equalized?
 user_input = input("Would you like to equalize the image? ")
@@ -198,6 +182,7 @@ if ans == 'y':
     cv2.namedWindow('After Equalization', cv2.WINDOW_KEEPRATIO)
     cv2.imshow("After Equalization", equalized)
 
+print("Hello World")
 
 cv2.waitKey()
 cv2.destroyAllWindows()
